285bbcd988a2f55f5bd143175115898fdd22dc32
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-06-11 10:43:01 -0500

    main buttons are changing view on click

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,17 +3,8 @@ import { loginWithGoogle } from '../auth';
 
 class SignIn extends Component {
 
-    constructor(props) {
-        super(props)
-    }
-
-
-
-
     // Launches authentication via Google
     authenticate(){
-
-        // const updateIt = this.props.changeAuth;
         // Login via Google and launch updateIt function in App.js 
         loginWithGoogle(this.props.userToApp)
     }
